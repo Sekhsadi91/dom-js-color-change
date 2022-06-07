f86d8183ce1c11edfe1d59553290a0c63627ff7a
Sekhsadi Islam <95013707+Sekhsadi91@users.noreply.github.com>
2022-06-07 00:11:16 -0700

    html js
    font-size,font-color,font-style ,font-weight,padding,box-shadow nice dom js.

---
+++ dom_color.js
@@ -0,0 +1,27 @@
+
+
+// function addStyle(){
+    
+//     let myStyle = document.querySelector("#paraId");
+//     // myStyle.style.color = "green";
+//     // myStyle.style.fontSize = "4rem";
+//     // myStyle.style.fontStyle = "italic";
+//     // myStyle.style.fontWeight = "bold";
+
+//     myStyle.classList.add("para-style");    
+// }
+
+// function removeStyle(){
+//     let myStyle = document.querySelector("#paraId");
+//     myStyle.classList.remove("para-style");
+// }
+
+function addStyle(){
+    let myStyle = document.querySelector("#paraId");
+    myStyle.classList.add("para-style");
+}
+
+function removeStyle(){
+    let myStyle = document.querySelector("#paraId");
+    myStyle.classList.remove("para-style");
+}
